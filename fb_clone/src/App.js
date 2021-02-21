18da1ddafebc0ecf57f4b62817bac29227aff2ac
marcfischer123467 <57159035+marcfischer123467@users.noreply.github.com>
2021-02-21 15:42:01 -0800

    clean up boiler plate

---
+++ fb_clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM css naming convention
+    <div className="app">
+     <h1>Let's build the facebook clone!</h1>
+    </div>
+  );
+}
+
+export default App;
