73066ffe53d721e0a17b72a57d2bef192942088a
marcfischer123467 <57159035+marcfischer123467@users.noreply.github.com>
2021-02-21 21:01:07 -0800

    posting

--- fb_clone/src/Feed.js
+++ fb_clone/src/Feed.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
 
 function Feed() {
     return (
         <div classname='feed'>
          <StoryReel />
-         {/* StoryReel */}
-         {/* MessageSender */}     
+         <MessageSender />
         </div>
     )
 }
