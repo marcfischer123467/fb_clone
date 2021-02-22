7052a13a45f467dd8b670f21ab09ef31f51c4f0a
marcfischer123467 <57159035+marcfischer123467@users.noreply.github.com>
2021-02-21 22:24:23 -0800

    break

--- fb_clone/src/Feed.js
+++ fb_clone/src/Feed.js
@@ -7,8 +7,17 @@ import Post from './Post'
 function Feed() {
     return (
         <div classname='feed'>
-         <StoryReel />
-         <MessageSender />
+            
+        <StoryReel />
+        <MessageSender />
+        <Post
+        profile Pic="https://media-exp1.licdn.com/dms/image/C4E03AQESovl90BIvpA/profile-displayphoto-shrink_200_200/0/1612487148599?e=1619654400&v=beta&t=w4PjkOGsDcPs1z-UXuyHLZl1hlA73WXmFJa8eMWsofc"
+        message= "Learning React and Firebase rn."
+        timestamp= "late"
+        username = 'Marc'
+        image=""
+        />
+         <Post />
          <Post />
         </div>
     )
